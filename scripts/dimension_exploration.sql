e7d7290dfaf897467e5939002f0c842fff7ac514
jennique264 <shwetas0738@gmail.com>
2026-06-16 18:56:35 +0530

    Create dimension_exploration.sql for data exploration
    Add SQL queries for exploring customer countries and product categories.

---
+++ scripts/dimension_exploration.sql
@@ -0,0 +1,11 @@
+-------------------------------------------------
+--2. Dimension Exploration
+-------------------------------------------------
+
+-- Explore all countries our customers come from
+SELECT DISTINCT country FROM gold.dim_customers
+
+
+-- Explore all categories 'The Major Divisions'
+SELECT DISTINCT category, subcategory, product_name FROM gold.dim_products
+ORDER BY 1,2,3
